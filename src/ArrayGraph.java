13870806d14bd616db4be076201b1afd08cf224f
Vanessa Kostadinova <vanessa_g_k@hotmail.com>
2021-10-11 23:47:37 +0100

    Implementation of tutorial 1.

--- src/ArrayGraph.java
+++ src/ArrayGraph.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class ArrayGraph {
@@ -17,23 +14,24 @@ public class ArrayGraph {
         List<Float[]> entries = new LinkedList<>();
 
         while (in.hasNext()) {
-            String str = in.next();
+            String str = in.nextLine();
             String[] values = commaRegex.split(str);
-            if (values.length == 3) {
+            if (values.length == 3 && Arrays.stream(values).noneMatch(e -> e.matches("[^0-9.]"))) {
                 entries.add(new Float[]{Float.parseFloat(values[1]), Float.parseFloat(values[2])});
             }
         }
-        int size = entries.size() - 1;
+
+        int size = entries.size();
         collection = new Double[size][size];
 
-        for (int i = 0; i <= size; i++) {
-            Float startX = entries.get(i)[1];
-            Float startY = entries.get(i)[2];
+        for (int i = 0; i < size; i++) {
+            Float startX = entries.get(i)[0];
+            Float startY = entries.get(i)[1];
 
-            for (int r = 0; r <= size; r++) {
+            for (int r = 0; r < size; r++) {
                 if (i != r) {
-                    Float endX = entries.get(r)[1];
-                    Float endY = entries.get(r)[2];
+                    Float endX = entries.get(r)[0];
+                    Float endY = entries.get(r)[1];
 
                     double distance = Math.sqrt((Math.pow((endX - startX), 2L) + Math.pow((endY - startY), 2L)));
                     collection[i][r] = distance;
@@ -43,13 +41,13 @@ public class ArrayGraph {
         }
     }
 
-    public Double walk(Integer[][] path) {
+    public Double walk(int[] path) {
         Double pathLength = 0D;
-        for (Integer[] coords : path) {
-            int start = coords[0];
-            int end = coords[1];
+        int previous = path[0];
 
-            pathLength += findLength(start, end);
+        for (int i = 1; i < path.length; i++) {
+            pathLength += findLength(previous, path[i]);
+            previous = path[i];
         }
 
         return pathLength;
@@ -59,10 +57,52 @@ public class ArrayGraph {
         return collection[start][end];
     }
 
-    public int[][] getAllValidRoutes() {
-        ArrayList<int[]> tempList = new ArrayList<>();
-        for (int start = 0; start < collection.length; start++) {
-            
+    public List<int[]> getAllValidRoutes() {
+        int[] temp = quickAndDirtyCombinationGeneration();
+        List<int[]> combinations = new ArrayList<>();
+
+        generate(temp.length, temp, combinations);
+
+        return combinations;
+    }
+
+    private int[] quickAndDirtyCombinationGeneration() {
+        int[] temp = new int[collection.length];
+
+        for (int i = 0; i < collection.length; i++) {
+            temp[i] = i;
+        }
+
+        return temp;
+    }
+
+    /**
+     * This method swaps the elements at the two indexes given in an array;
+     */
+    private void swap(int[] array, final int indexA, final int indexB) {
+        int a = array[indexA];
+        array[indexA] = array[indexB];
+        array[indexB] = a;
+    }
+
+    private void generate(int len, int[] array, List<int[]> out) {
+        if (len == 1) {
+            int [] finalArray = Arrays.copyOf(array, array.length + 1);
+            finalArray[array.length] = array[0];
+            out.add(finalArray);
+            return;
+        }
+
+        generate(len - 1, array, out);
+
+        for (int i = 0; i < len - 1; i++) {
+            if (len % 2 == 0) {
+                swap(array, i, len - 1);
+            } else {
+                swap(array, 0, len - 1);
+            }
+
+            generate(len - 1, array, out);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Main {
 
+    static ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+    static Double[][] test = new Double[][]{
+            {5D,1D,2D},
+            {2D,4D,1D},
+            {1D,4D,2D}
+    };
 
     public static void main(String[] args) {
 
+        try(Scanner sc = new Scanner(loadFile("ulysses16(1).csv"))) {
+            ArrayGraph graph = new ArrayGraph(sc);
+            Double lowestTotal = null;
+
+            for(int[] i : graph.getAllValidRoutes()){
+                Double cost = graph.walk(i);
+                if (lowestTotal == null || cost < lowestTotal){
+                    lowestTotal = cost;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static File loadFile(String name) throws FileNotFoundException {
+        URL path = classloader.getResource(name);
+        if(path != null){
+            return new File(path.getPath());
+        } else {
+            throw new FileNotFoundException(name);
+        }
     }
 }
